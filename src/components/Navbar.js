c2b24d021e92af3d192dbbfceddda25ec17caa7c
d3vboi <d3vboi@proton.me>
2023-07-29 23:21:31 +0200

    Rounded Navbar buttons and added Profile Picture Icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
 import './Navbar.css'
+import PfpLogo from '../imgs/user.png'
+
+
+
 const Navbar =()=>{
       return (
             <div class="navbar">
                   <Link className="link" to="/">Synthia AI</Link>
-                  <Link style={{float:'right'}} className="link" id="profile">Acc</Link>
+                  <Link className="link" id="profile"><img src={PfpLogo} alt="Profile Picture"/></Link>
             </div>
       )
 }
